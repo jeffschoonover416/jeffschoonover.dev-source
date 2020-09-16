387c5a17ea4359d0495d0d24a90c42de38408b1b
Jeff Schoonover <jeffschoonover416@gmail.com>
2020-09-16 00:38:54 -0400

    fix flash with scully transfer-state

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,35 +1,47 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ChangeDetectionStrategy } from '@angular/core';
+import { Observable } from 'rxjs';
+import {
+  ScullyRoute,
+  ScullyRoutesService,
+  TransferStateService,
+} from '@scullyio/ng-lib';
 import { map } from 'rxjs/operators';
-import { ScullyRoutesService, ScullyRoute } from '@scullyio/ng-lib';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class HomeComponent {
+  blogPosts$: Observable<any>;
 
   constructor(
-    private scully: ScullyRoutesService) {}
-  
-    $blogPosts = this.scully.available$.pipe(
-      //Start with array of all available (publish=true) routes
-      map((routes: ScullyRoute[]) =>  
-        routes.filter(
-          //Look at each route in the array, and keep only .md files with /posts/ route
-          //(that is all the blog posts)
-          //sourcefile? has the question mark because some routes might not have a sourcefile
-          (route: ScullyRoute) => 
-          route.route.startsWith('/posts/') && route.sourceFile?.endsWith('.md')
-        )
-      ),
-      //Sort the array of filtered routes in descending order *before* passing it to the 
-      //async pipe in the html code
-      map((filteredRoutes: ScullyRoute[]) => {
-        return filteredRoutes.sort( (postA: ScullyRoute, postB: ScullyRoute) => {
-          return ((+new Date(postB['date'])) - (+new Date(postA['date'])));
-        });
-      }),
-    );
+    private scully: ScullyRoutesService,
+    private transferStateService: TransferStateService
+  ) {
+    this.blogPosts$ = this.transferStateService.useScullyTransferState(
+      'blogPostRoutes',
+      this.scully.available$.pipe(
+        //Start with array of all available (publish=true) routes
+        map((routes: ScullyRoute[]) =>  
+          routes.filter(
+            //Look at each route in the array, and keep only .md files with /posts/ route
+            //(that is all the blog posts)
+            //sourcefile? has the question mark because some routes might not have a sourcefile
+            (route: ScullyRoute) => 
+            route.route.startsWith('/posts/') && route.sourceFile?.endsWith('.md')
+          )
+        ),
+        //Sort the array of filtered routes in descending order *before* passing it to the 
+        //async pipe in the html code
+        map((filteredRoutes: ScullyRoute[]) => {
+          return filteredRoutes.sort( (postA: ScullyRoute, postB: ScullyRoute) => {
+            return ((+new Date(postB['date'])) - (+new Date(postA['date'])));
+          });
+        }),
+      ));  
+    
+  }
 
 }
