7b02bb95788c75c4edcd4459804593059d39b113
Jeff Schoonover <jeffschoonover416@gmail.com>
2020-09-16 01:13:47 -0400

    removed post list from app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,4 @@
 import { Component } from '@angular/core';
-import { map } from 'rxjs/operators';
-import { ScullyRoutesService, ScullyRoute } from '@scullyio/ng-lib';
 
 @Component({
   selector: 'app-root',
@@ -10,26 +8,6 @@ import { ScullyRoutesService, ScullyRoute } from '@scullyio/ng-lib';
 export class AppComponent {
   title = 'jsdevblog';
 
-  constructor(
-    private scully: ScullyRoutesService) {}
+  constructor() {}
   
-    $blogPosts = this.scully.available$.pipe(
-      //Start with array of all available (publish=true) routes
-      map((routes: ScullyRoute[]) =>  
-        routes.filter(
-          //Look at each route in the array, and keep only .md files with /posts/ route
-          //(that is all the blog posts)
-          //sourcefile? has the question mark because some routes might not have a sourcefile
-          (route: ScullyRoute) => 
-          route.route.startsWith('/posts/') && route.sourceFile?.endsWith('.md')
-        )
-      ),
-      //Sort the array of filtered routes in descending order *before* passing it to the 
-      //async pipe in the html code
-      map((filteredRoutes: ScullyRoute[]) => {
-        return filteredRoutes.sort( (postA: ScullyRoute, postB: ScullyRoute) => {
-          return ((+new Date(postB['date'])) - (+new Date(postA['date'])));
-        });
-      }),
-    );
-}
+}    
