8e71f48e215d93993bd5e53113fac606c5543486
Jeff Schoonover <jeffschoonover416@gmail.com>
2020-10-17 22:39:55 -0400

    baseline: no inline CSS

--- scully.jsweb.config.ts
+++ scully.jsweb.config.ts
@@ -1,11 +1,15 @@
 import { ScullyConfig, setPluginConfig } from '@scullyio/scully';
+import { criticalCSS } from '@scullyio/scully-plugin-critical-css';
 import 'prismjs/components/prism-yaml.js';
+
+const defaultPostRenderers = ['seoHrefOptimise', criticalCSS];
 
 setPluginConfig('md',  { enableSyntaxHighlighting: true });
 
 export const config: ScullyConfig = {
   projectRoot: "./src",
   projectName: "jsweb",
+  //defaultPostRenderers,
   outDir: './dist/static',
   routes: {
     '/posts/:id': {
